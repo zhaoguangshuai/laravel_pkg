c7ef5ee292c359399753e6b7365339323a83ff46
zhaoguangshuai <969426998zgs@gmail.com>
2020-04-20 07:22:06 +0000

    cs

--- app/Repositories/User/AuthRepository.php
+++ app/Repositories/User/AuthRepository.php
@@ -14,12 +14,22 @@ class AuthRepository extends BaseRepository
         $phone = $request->input('phone');
         $pass_word = $request->input('pass_word');
         $user_info = User::query()->where('phone', $phone)->first();
+
+
         if (empty($user_info)) return $this->failed('该用户不存在');
 
-        $token = Auth::guard('api')->setTTL(604800)->fromUser($user_info);
+        $token = Auth::guard('api')->setTTL(1)->fromUser($user_info);
+
+        //$payload = Auth::guard('api')->payload();
+        //dd($payload->get('sub'));
 
         //让上一个token过期
-        if ($user_info->last_token) JWTAuth::setToken($user_info->last_token)->invalidate();
+        if ($user_info->last_token) {
+            //提前判断该token是否过期，过期了就不用执行下面的操作了
+            //if (auth()->validate($user_info->last_token)) {
+                Auth::guard('api')->setToken($user_info->last_token)->invalidate();
+            //}
+        }
         //将新的token存储到数据库
         $user_info->last_token = $token;
         $user_info->save();
