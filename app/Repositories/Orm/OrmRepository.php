d8ee5efe1102f8832edffac2a25675254e564e0a
username <email>
2021-01-11 04:15:07 +0000

    高级子查询

--- app/Repositories/Orm/OrmRepository.php
+++ app/Repositories/Orm/OrmRepository.php
@@ -37,7 +37,7 @@ class OrmRepository extends BaseRepository
 //        !empty($data['deptname']) && $query->whereLike('dep.deptname', "%{$data['deptname']}%");
 //        !empty($data['jobname']) && $query->whereLike('pos.jobname', "%{$data['jobname']}%");
 
-        $data = ['title1' => 5, 'title2' => 1];
+        //$data = ['title1' => 5, 'title2' => 1];
 
 //        $builder = AuctionPlays::query()
 //            ->with([
@@ -58,49 +58,43 @@ class OrmRepository extends BaseRepository
 //            }
 //        ]);
 
-        $data = ['title1' => 5, 'title2' => 2];
-        $builder = AuctionPlays::query()
-            ->with([
-                'auctionGoods' => function ($query1) {
-                    $query1->select('title', 'sort', 'play_id', 'id', 'art_org_id')
-                        ->whereIn('art_org_id', [1, 5])
-                        ->with([
-                            'auctionHistories' => function ($query2) {
-                                $query2->select('user_name', 'user_img', 'auction_good_id')
-                                    ->where('auction_good_id', 1);
-
-                                //!empty($data['title2']) && $builder_two->where('auction_good_id', 'like', '%'.$data['title2'].'%');
-                            }
-                        ]);
-
-                    //!empty($data['title1']) && $builder_one->where('art_org_id', 'like', '%'.$data['title1'].'%');
-
-                }
-            ]);
-
-        $builder->whereHas('auctionGoods', function ($query1) use ($data) {
-            if (!empty($data['title1'])) {
-                $query1->where('art_org_id', 'like', '%'.$data['title1'].'%');
-            }
-
-            $query1->whereHas('auctionHistories', function ($query2) use ($data) {
-                if (!empty($data['title2'])) {
-                    $query2->where('auction_good_id', 'like', '%'.$data['title2'].'%');
-                }
-            });
-
-        });
-
-
-        $data = $builder->limit(10)->get();
-
-        return $this->success($data);
-
-
-
-
-
-
+//        $data = ['title1' => 5, 'title2' => 2];
+//        $builder = AuctionPlays::query()
+//            ->with([
+//                'auctionGoods' => function ($query1) {
+//                    $query1->select('title', 'sort', 'play_id', 'id', 'art_org_id')
+//                        ->whereIn('art_org_id', [1, 5])
+//                        ->with([
+//                            'auctionHistories' => function ($query2) {
+//                                $query2->select('user_name', 'user_img', 'auction_good_id')
+//                                    ->where('auction_good_id', 1);
+//
+//                                //!empty($data['title2']) && $builder_two->where('auction_good_id', 'like', '%'.$data['title2'].'%');
+//                            }
+//                        ]);
+//
+//                    //!empty($data['title1']) && $builder_one->where('art_org_id', 'like', '%'.$data['title1'].'%');
+//
+//                }
+//            ]);
+//
+//        $builder->whereHas('auctionGoods', function ($query1) use ($data) {
+//            if (!empty($data['title1'])) {
+//                $query1->where('art_org_id', 'like', '%'.$data['title1'].'%');
+//            }
+//
+//            $query1->whereHas('auctionHistories', function ($query2) use ($data) {
+//                if (!empty($data['title2'])) {
+//                    $query2->where('auction_good_id', 'like', '%'.$data['title2'].'%');
+//                }
+//            });
+//
+//        });
+//
+//
+//        $data = $builder->limit(10)->get();
+//
+//        return $this->success($data);
 
 
 //        AuctionPlays::query()->chunk(200, function ($query) {
@@ -109,17 +103,17 @@ class OrmRepository extends BaseRepository
 //           }
 //        });
 
-//        $res = AuctionPlays::query()
-//            ->addSelect(['last_goods_id' =>
-//            AuctionGoods::query()
-//                ->select('id')
-//                ->whereColumn('play_id', 'auction_plays.id')
-//                ->orderBy('id', 'desc')->first()->toArray()
-//            ])
-//            ->limit(10)
-//            ->get();
+        $res = AuctionPlays::query()
+            ->addSelect(['last_goods_id' =>
+            AuctionGoods::query()
+                ->select('id')
+                ->whereColumn('play_id', 'auction_plays.id')
+                ->orderBy('id', 'desc')->first()->toArray()
+            ])
+            ->limit(10)
+            ->get();
 
-        //return $this->success($res);
+        return $this->success($res);
     }
 
 
